91c67fb5498571f7e45e2be9af81671a5679f69d
Sarah Hick <sarah0032@hotmail.com>
2018-03-25 13:41:25 +0200

    Creation of a grid from array's content

--- js/app.js
+++ js/app.js
@@ -1,18 +1,62 @@
+/******************
+*******************
+      GLOBAL
+*******************
+******************/
 
 const cards = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
-// doubling the cards for the memory
-cards.forEach(function(card) {
-   cards.push(card);
-});
+
+/******************
+*******************
+   SHUFFLE
+*******************
+******************/
 
 //shuffles the array -> modern version of the Fisher–Yates shuffle algorithm
-function shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
+function shuffle(el) {
+    for (let i = 0; i < el.length; i++) {
         const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
+        [el[i], el[j]] = [el[j], el[i]];
     }
-    return a;
+    return el;
 }
 
-shuffle(el);
+
+
+/******************
+*******************
+   MAKING GRID
+*******************
+******************/
+
+//insert the cards in the grid section
+function appendCards(el) {
+
+   const grid = document.getElementById('js-grid');
+   const fragment = document.createDocumentFragment();
+
+   for (let i = 0; i < el.length; i++) {
+      const newDiv = document.createElement('div');
+      // add the content of the array's element in the div
+      let divContent = el[i];
+      newDiv.innerText = divContent ;
+      fragment.appendChild(newDiv);
+   }
+
+   grid.appendChild(fragment);
+}
+
+
+function makeGrid(el) {
+
+   // doubling the cards for the memory game
+   cards.forEach(function(card) {
+      cards.push(card);
+   });
+
+   shuffle(el);
+   appendCards(el);
+}
+
+makeGrid(cards);
