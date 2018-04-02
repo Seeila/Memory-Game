3ca71e4f95e568ef6d9a0387581769f0bc563980
Sarah Hick <sarah0032@hotmail.com>
2018-04-02 20:55:59 +0200

    bug of accelarating timer fixed

--- js/app.js
+++ js/app.js
@@ -52,6 +52,7 @@ function chooseLvlModal() {
 }
 
 function startLvl(evt) {
+      clearInterval(timerID);
    // Firefox 1.0+
    const isFirefox = typeof InstallTrigger !== 'undefined';
    let inputVal;
